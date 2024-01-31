bed04ecba8c00d94594cbed413d45248c6b4db01
taichanne30 <dev@taichan.site>
2024-01-31 22:43:44 +0900

    First release

---
+++ content/docs/index.md
@@ -0,0 +1,11 @@
+---
+title: ドキュメント
+description: なですきーのドキュメントです。
+category: docs
+tags:
+  - ルール
+  - 規約
+---
+
+# docs
+- [利用規約](/terms)
---
+++ pages/[...slug].vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <ContentDoc />
+  </main>
+</template>
+<style>
+  h2 {
+    text-decoration: none;
+  }
+  main {
+    width: 100%;
+  }
+</style>
