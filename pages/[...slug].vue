245460d7097d4396698e78722f7787dc0e175b0b
taichanne30 <dev@taichan.site>
2024-03-07 16:48:44 +0900

    ContentDOc

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,10 +1,16 @@
 <template>
   <main>
-    test
     <ContentDoc>
+      <template #default="{ doc }">
+        <ContentRenderer :value="doc" />
+      </template>
       <template #not-found>
         <h1>404 Not Found</h1>
         <p>アクセスしたページが見つかりませんでした。</p>
+      </template>
+      <template #empty>
+        <h1>Empty</h1>
+        <p>このページは空です</p>
       </template>
     </ContentDoc>
   </main>
